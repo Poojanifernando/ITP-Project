4893c974dd4205141b5c0c6c33d9216bf4fe30d2
HP <IT20156374@my.sliit.lk>
2021-09-11 18:11:35 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express =require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const app =express();
+
+//import routes
+
+
+
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//route middleware
+
+
+
+
+const PORT = 8000;
+const DB_URL ='mongodb+srv://AnodyaFernando:Poojani@@1234@project.shu8e.mongodb.net/BusOwner?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log('DB connected');
+})
+.catch((err)=> console.log('DB Connection Error',err));
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log('App is running on ${PORT}');
+});

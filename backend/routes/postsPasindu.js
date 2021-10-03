7f7414d97c4ef23c39c716e0b8d7badf9d28bf72
Pasindu Udara <udarabandara19@gamil.com>
2021-10-03 16:55:13 +0530

    pasindu backend

---
+++ backend/routes/postsPasindu.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const posts = require('../models/postsPasindu');
+
+const router = express.Router();
+
+//save posts
+
+router.post('/postPasindu/save',(req,res)=>{
+
+    let newPost = new posts(req.body);
+
+    newPost.save((err)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:"Post saved successfully"
+        });
+    });
+});
+
+//getpost
+router.get('/postsPasindu',(req,res)=>{
+    posts.find().exec((err,post)=>{
+        if(err){
+            return res.status(400).json({
+            error:err    
+            });
+        }
+        return res.status(200).json({
+            success:true,
+            existingPosts:post
+        });
+    });
+});
+
+//get a specific post
+
+router.get("/postPasindu/:id",(req,res) =>{
+    let postId = req.params.id;
+
+    posts.findById(postId,(err,post) =>{
+      if(err){
+          return res.status(400).jason({success:false,err});
+        
+      } 
+         return res.status(200).json({
+             success:true,
+             post
+         });
+    });
+});
+
+
+
+
+//update posts
+
+router.put('/post/updatePasindu/:id',(req,res)=>{
+    posts.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,post)=>{
+            if(err){
+                return res.status(400).jason({error:err});
+            }
+
+            return res.status(200).json({
+                success:"Updated Succesfully"
+            });
+        }
+    );
+});
+
+
+//delete posts
+
+router.delete('/post/deletePasindu/:id',(req,res)=>{
+    posts.findByIdAndRemove(req.params.id).exec((err,deletedPost) => {
+
+        if(err) return res.status(400).json({
+            message:"Delete unsuccesful",err
+        });
+
+        return res.json({
+            message:"Delete Succesfull",deletedPost
+        });
+    });
+});
+
+module.exports = router;
+
+
+

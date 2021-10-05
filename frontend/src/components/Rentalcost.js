01c6d85f23017ab85f7ee94971f812a2c619b239
HP <IT20156374@my.sliit.lk>
2021-10-05 16:23:47 +0530

    changed nav bar

--- frontend/src/components/Rentalcost.js
+++ frontend/src/components/Rentalcost.js
@@ -76,24 +76,7 @@ export default class Rentalcost extends Component{
       <div style={{backgroundColor: "#080523"}}>
           <div>
 
-          <nav className="navbar navbar-light navbar-expand-md bg-dark navigation-clean-search">
-          <div className="container"><a className="navbar-brand" href="#" style={{fontsize: '19px',color:'rgb(255,255,255)'}}>Travelling Express</a><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
-              <div className="collapse navbar-collapse" id="navcol-1">
-                  <ul className="navbar-nav">
-                      <li className="nav-item"><a className="nav-link "href="#" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Dashboard</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Profile</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Drivers</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Scheduled Trips</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Special Services</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Company Bus Details </a></li>
-                      <li className="nav-item"><a className="nav-link" href="/cost" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Rental Bus Details </a></li>
-                      <li className="nav-item"><a className="nav-link" href="/homepage" style={{fontsize:'15px',color:'rgb(255,255,255)'}}>Manage Locations</a></li>
-                  </ul>
-                  
-                  <a className="btn btn-light action-button" role="button" href="#" style={{fontsize:'15px', background: 'rgb(69,27,158)' ,color: 'rgb(255,255,255)'}}>Logout</a>
-              </div>
-          </div>
-      </nav>
+       
             </div>
       
 

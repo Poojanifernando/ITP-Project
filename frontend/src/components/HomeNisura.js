32d742c5f88461a86f5c7213964e8dfd78468762
Nisura-00 <it20133504@my.sliit.lk>
2021-10-09 21:15:44 +0530

    nisuu

--- frontend/src/components/HomeNisura.js
+++ frontend/src/components/HomeNisura.js
@@ -142,6 +142,52 @@ export default class HomeNisura extends Component {
         </table>
         <button className="btn btn-success"><a href="/niss" style={{textDecoration:'none', color:'white'}}>Create New Booking</a></button> 
       </div>
+
+      <footer class="page-footer font-small cyan darken-3" style={{backgroundColor:"black", marginTop:"00px",height:"170px"}}>
+
+<div>
+
+    <div class="row" style={{marginLeft:"525px",height:"100px"}}>
+
+    <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+
+        <a class="fb-ic" style={{paddingLeft:"10px"}}>
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        
+        <a class="tw-ic"  style={{paddingLeft:"20px"}}>
+            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        
+        <a class="gplus-ic" style={{paddingLeft:"20px"}}>
+            <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        
+        <a class="li-ic" style={{paddingLeft:"20px"}}>
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+    
+        <a class="ins-ic" style={{paddingLeft:"20px"}}>
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        
+        <a class="pin-ic" style={{paddingLeft:"20px"}}>
+            <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+        </a>
+        </div>
+    </div>
+
+    </div>
+
+</div>
+
+<div class="footer-copyright text-center py-3" style={{color:"white"}}>© 2021 Copyright:
+    <a href="https://google.com/"> Travelling Express</a>
+</div>
+
+</footer>
+
       </div>
     )
      
